b69a577657f5309c97f8dd2b78b0d6b30bbb9b7b
DEEPAK6265 <deepaksharma934029@gmail.com>
2022-04-23 18:30:01 +0530

    practicequestion

---
+++ pnameplace.js
@@ -0,0 +1,17 @@
+var P="Ashish"
+if (P=="Ashish")
+{
+    console.log("Bhopal");
+}
+ else if(P=="Deepak")
+{
+    console.log("Rewa");
+}
+else if(P=="Shubham")
+{
+    console.log("Prayagraj")
+}
+else
+{
+   console.log ("Person not found");
+}
