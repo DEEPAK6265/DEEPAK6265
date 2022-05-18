75aede39804e4f75b8b0015b3af2fa1ea1c6a047
DEEPAK6265 <deepaksharma934029@gmail.com>
2022-05-18 17:08:43 +0530

    Practice

---
+++ function.js
@@ -0,0 +1,11 @@
+function Cookmaggie (maggie,pani,tapeli)
+{
+    console.log(" your maggie will be ready in "
+    +maggie*2
+    +" minutes  "
+    +"and ingrediants used are:- "
+    + maggie +" maggie "
+   +pani+" cups of water "
+    +" using " +tapeli+" pan ")
+}
+Cookmaggie(4,8,2)
